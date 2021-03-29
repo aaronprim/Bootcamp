1683517bfde9c4549302a8ecb7ec3bb58b8390c4
aaronprim <aaronmprim@gmail.com>
2021-03-28 21:57:34 -0400

    initial commit

---
+++ Week1&2/JS/Loop_Challenges.js
@@ -0,0 +1,16 @@
+for (x=1; x<=20; x++1) ;
+    if (x % 2 != 0) {
+        console.log(x+ " ")
+    }
+Arr [4, 2.5, 1, -0.5, -2]
+    console.log(arr)
+
+sum = x
+    (x=1; x<=100; x++1);
+    console.log(sum + x)
+
+
+
+
+
+    
