35c5c57ace314f62c7e2ef6f82cb029f3eb8b005
aaronprim <aaronmprim@gmail.com>
2021-04-06 14:30:03 -0400

    initial commit

---
+++ python/fundamentals/fundamentals/greetings.py
@@ -0,0 +1,24 @@
+# # Part 1
+# greeting = input("What is your name?")
+# print(f'Your name is: {greeting}')
+
+# # Part 2
+# greeting = input("What is your name?")
+# if ( greeting == "Aaron"):    
+#     print("Hey thats my name too!")
+# else:
+#     print(f"Your name is: {greeting}")
+
+
+# Part 3
+names= []
+greeting = ''
+while names in range (0, 10):
+    greeting = (input("What is your name?"))
+    names.append(greeting)
+    if i == 10:
+        print("Nice to meet all of you")
+print (names)
+
+
+
