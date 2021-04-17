b3e6c4328a781a027792f98d375bf4edbdee2d8c
aaronprim <aaronmprim@gmail.com>
2021-04-17 17:36:02 -0400

    initial commit

---
+++ python/flask/fundamentals/dojo_survey/server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def survey():
+    return render_template("form.html")
+
+
+@app.route('/results',methods = ['POST'])
+def submit():
+    print(request.form)
+    name_from_form = request.form['name']
+    dojo_from_form = request.form['dojo']
+    lang_from_form = request.form['language']
+    com_from_form = request.form['comment']
+
+    return redirect(render_template("result.html", name_=name_from_form, dojo_=dojo_from_form, lang_=lang_from_form, com_=com_from_form))
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+@app.route('/result')
+def result():
+    return render_template(result.html)
