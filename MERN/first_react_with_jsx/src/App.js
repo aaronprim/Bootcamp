faaa5602d4495937b0498cb4c4fc83eed2ab07ff
aaronprim <aaronmprim@gmail.com>
2021-05-18 11:11:15 -0400

    initial commit

---
+++ MERN/first_react_with_jsx/src/App.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> 
+      Hello Dojo!
+      </h1>
+      <h2>
+        Things I need to do:
+      </h2>
+      <ul>
+        <li> Learn React</li>
+      </ul>
+    </div>
+  );
+
+}
+
+
+
+
+export default App;
