3f83a692e8c2c780e72d0d20a585d77b0f64c306
aaronprim <aaronmprim@gmail.com>
2021-06-08 13:11:31 -0400

    initial commit

---
+++ java/javaFun/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz fB = new FizzBuzz();
+        String outputOne = fB.fizzBuzz(3);
+        System.out.println(outputOne);
+        String outputTwo = fB.fizzBuzz(5);
+        System.out.println(outputTwo);
+        String outputThree = fB.fizzBuzz(15);
+        System.out.println(outputThree);
+        String outputFour = fB.fizzBuzz(2);
+        System.out.println(outputFour);
+    }
+}
